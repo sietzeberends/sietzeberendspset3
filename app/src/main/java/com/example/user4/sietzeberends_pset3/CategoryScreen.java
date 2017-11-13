343dd01194f45a2368efec812d7419c1e2d9d409
Sietze Berends <sietze.berends@gmail.com>
2017-11-13 13:42:15 +0100

    Initial commit

---
+++ app/src/main/java/com/example/user4/sietzeberends_pset3/CategoryScreen.java
@@ -0,0 +1,13 @@
+package com.example.user4.sietzeberends_pset3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CategoryScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category_screen);
+    }
+}
